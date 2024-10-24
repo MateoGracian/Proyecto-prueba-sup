dff6522602c556271bb16e59b06e1b675608b7e2
carlos <carlos.figueira1212@gmail.com>
2024-10-23 21:42:14 -0400

    Componente Carlos Figueira

---
+++ vite-project/src/components/Carlos/ComponentCarlos.jsx
@@ -0,0 +1,11 @@
+import styles from "./ComponentCarlos.module.css"
+
+const ComponentCarlos = () => {
+
+    return(
+        <div className={styles.componentContainer}>
+            <h2 className={style.title}> Componente de Carlos</h2>
+        </div>
+    )
+}
+export default ComponentCarlos;
